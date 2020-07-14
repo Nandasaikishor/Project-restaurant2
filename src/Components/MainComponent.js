fbcdf06751c390043482b1fe020aca2acddfff02
Nanda sai kishor <nandasaikishore1@gmail.com>
2020-07-14 20:25:08 +0530

    Single page application part - 2

--- src/Components/MainComponent.js
+++ src/Components/MainComponent.js
@@ -22,6 +22,7 @@ class Main extends Component{
          leaders: LEADERS
        };
     }
+  
 
     render() {
       const HomePage = () => {
@@ -33,12 +34,19 @@ class Main extends Component{
       />
         );
       }
+      const DishWithId = ({match}) => {
+        return(
+            <DishDetail dish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]} 
+              comments={this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))} />
+        );
+      };
       return (
         <div>
           <Header />
           <Switch>
             <Route path="/home" component={HomePage} />
             <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes} />} />
+            <Route path='/menu/:dishId' component={DishWithId} />
             <Route exact path ="/contactus" component={Contact}/>
             <Redirect to="/home" />
           </Switch>
--- src/Components/dishDetailComponent.js
+++ src/Components/dishDetailComponent.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import { Card, CardImg, CardBody, CardText, CardTitle } from 'reactstrap';
+import { Card, CardImg, CardText, CardBody,
+    CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
    function RenderComments({comments}) {
         if (comments == null) {return(<div></div>);}
@@ -19,7 +21,7 @@ import { Card, CardImg, CardBody, CardText, CardTitle } from 'reactstrap';
             );
         });
         return (
-            <div className='col-12 col-md-5 m-1'>
+            <div className='col-12 m-1'>
                 <h1> Comments </h1>
                 <ul className = "list-unstyled">{cmnts}</ul>
             </div>
@@ -29,7 +31,7 @@ import { Card, CardImg, CardBody, CardText, CardTitle } from 'reactstrap';
    function RenderDish({dish}) {
         if (dish != null) {
             return (
-                <div className='col-12 col-md-5 m-1'>
+                <div className='col-12 m-1'>
                     <Card>
                         <CardImg width="100%" src={dish.image} alt={dish.name} />
                         <CardBody>
@@ -45,11 +47,25 @@ import { Card, CardImg, CardBody, CardText, CardTitle } from 'reactstrap';
     const DishDetail = (props) => {
         if (props.dish != null){
             return (
-                <div className = "container">
-                    <div className = "row">
-                    <RenderDish dish = {props.dish}/>,
-                    <RenderComments comments = {props.dish.comments}/>
+                <div className="container">
+                <div className="row">
+                    <Breadcrumb>
+                        <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
+                        <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+                    </Breadcrumb>
+                    <div className="col-12">
+                        <h3>{props.dish.name}</h3>
+                        <hr />
+                    </div>                
+                </div>
+                <div className="row">
+                    <div className="col-12 col-md-5 m-1">
+                        <RenderDish dish={props.dish} />
                     </div>
+                    <div className="col-12 col-md-5 m-1">
+                        <RenderComments comments={props.comments} />
+                    </div>
+                </div>
                 </div>
             );
         } 
